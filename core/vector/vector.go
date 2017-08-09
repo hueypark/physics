d2997e6978dbed1a3d62b8222bfadfe47cf81256
HueyPark <jaewan.huey.park@gmail.com>
2017-08-09 20:14:21 +0900

    Delete Y axis from vector

--- core/vector/vector.go
+++ core/vector/vector.go
@@ -5,13 +5,11 @@ import "math"
 type Vector struct {
 	X float64
 	Y float64
-	Z float64
 }
 
 func (v *Vector) Invert() {
 	v.X = -v.X
 	v.Y = -v.Y
-	v.Z = -v.Z
 }
 
 func (v *Vector) Magnitude() float64 {
@@ -19,7 +17,7 @@ func (v *Vector) Magnitude() float64 {
 }
 
 func (v *Vector) SquareMagnitude() float64 {
-	return (v.X * v.X) + (v.Y * v.Y) + (v.Z * v.Z)
+	return (v.X * v.X) + (v.Y * v.Y)
 }
 
 func (v *Vector) Normalize() {
@@ -27,51 +25,38 @@ func (v *Vector) Normalize() {
 	if l > 0 {
 		v.X /= l
 		v.Y /= l
-		v.Z /= l
 	}
 }
 
 func (v *Vector) Multiply(val float64) {
 	v.X *= val
 	v.Y *= val
-	v.Z *= val
 }
 
 func (v *Vector) Add(o Vector) {
 	v.X += o.X
 	v.Y += o.Y
-	v.Z += o.Z
 }
 
 func (v *Vector) AddScaledVector(o Vector, scale float64) {
 	v.X += o.X * scale
 	v.Y += o.Y * scale
-	v.Z += o.Z * scale
 }
 
 func (v *Vector) Subtract(o Vector) {
 	v.X -= o.X
 	v.Y -= o.Y
-	v.Z -= o.Z
 }
 
 func (v *Vector) Clear() {
 	v.X = 0
 	v.Y = 0
-	v.Z = 0
 }
 
 func Product(lhs, rhs Vector) Vector {
-	return Vector{lhs.X * rhs.X, lhs.Y * rhs.Y, lhs.Z * rhs.Z}
+	return Vector{lhs.X * rhs.X, lhs.Y * rhs.Y}
 }
 
 func Dot(lhs, rhs Vector) float64 {
-	return (lhs.X * rhs.X) + (lhs.Y * rhs.Y) + (lhs.Z * rhs.Z)
-}
-
-func Cross(lhs, rhs Vector) Vector {
-	return Vector{
-		(lhs.Y * rhs.Z) - (lhs.Z * rhs.Y),
-		(lhs.Z * rhs.X) - (lhs.X * rhs.Z),
-		(lhs.X * rhs.Y) - (lhs.Y * rhs.X)}
+	return (lhs.X * rhs.X) + (lhs.Y * rhs.Y)
 }
--- core/vector/vector_test.go
+++ core/vector/vector_test.go
@@ -8,51 +8,35 @@ import (
 func TestNormalize(t *testing.T) {
 	a := assert.New(t)
 
-	v1 := Vector{10, 0, 0}
+	v1 := Vector{10, 0}
 	v1.Normalize()
 
-	nV1 := Vector{1, 0, 0}
+	nV1 := Vector{1, 0}
 	a.Equal(v1, nV1)
 
-	v2 := Vector{3, 4, 0}
+	v2 := Vector{3, 4}
 	v2.Normalize()
 
-	nV2 := Vector{0.6, 0.8, 0}
+	nV2 := Vector{0.6, 0.8}
 	a.Equal(v2, nV2)
 }
 
 func TestProduct(t *testing.T) {
 	a := assert.New(t)
 
-	v1 := Vector{1,2,3}
-	v2 := Vector{4,5,6}
+	v1 := Vector{1,2}
+	v2 := Vector{4,5}
 	v3 := Product(v1, v2)
 
-	a.Equal(v3, Vector{4, 10, 18})
+	a.Equal(v3, Vector{4, 10})
 }
 
 func TestDot(t *testing.T) {
 	a := assert.New(t)
 
-	v1 := Vector{1,2,3}
-	v2 := Vector{4,5,6}
+	v1 := Vector{1,2}
+	v2 := Vector{4,5}
 	dot := Dot(v1, v2)
 
-	a.Equal(dot, 32.0)
-}
-
-func TestCross(t *testing.T) {
-	a := assert.New(t)
-
-	v1 := Vector{1, 0, 0}
-	v2 := Vector{0, 1, 0}
-	cross := Cross(v1, v2)
-
-	a.Equal(cross, Vector{0,0,1})
-
-	v3 := Vector{1,0,0}
-	v4 := Vector{2,0,0}
-	cross2 := Cross(v3, v4)
-
-	a.Equal(cross2, Vector{0,0,0})
+	a.Equal(dot, 14.0)
 }
