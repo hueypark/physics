2bee28ad9a463edd845dc9e0599692c2245b3bd1
HueyPark <jaewan.huey.park@gmail.com>
2017-08-09 22:14:15 +0900

    Rename engine to world

--- core/world.go
+++ core/world.go
@@ -4,24 +4,24 @@ type Actor interface {
 	Tick(delta float64)
 }
 
-type Engine struct {
+type World struct {
 	actors map[int64]Actor
 }
 
-func New() Engine {
-	return Engine{make(map[int64]Actor)}
+func New() World {
+	return World{make(map[int64]Actor)}
 }
 
-func (e *Engine) Tick(delta float64) {
+func (e *World) Tick(delta float64) {
 	for _, object := range e.actors {
 		object.Tick(delta)
 	}
 }
 
-func (e *Engine) Add(a Actor) {
+func (e *World) Add(a Actor) {
 	e.actors[Context.IdGenerator.Generate()] = a
 }
 
-func (e *Engine) GetObjects() map[int64]Actor {
+func (e *World) GetObjects() map[int64]Actor {
 	return e.actors
 }
