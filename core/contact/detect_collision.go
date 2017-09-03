cce49fc90b81308342c10d37adfe3cd1cf80989f
hueypark <jaewan.huey.park@gmail.com>
2017-09-03 13:49:08 +0900

    Create detect collision method signature

--- core/contact/detect_collision.go
+++ core/contact/detect_collision.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hueypark/physics/core/shape"
 	"github.com/hueypark/physics/core/shape/circle"
+	"github.com/hueypark/physics/core/shape/convex"
 	"github.com/hueypark/physics/core/vector"
 )
 
@@ -14,6 +15,12 @@ func (c *Contact) DetectCollision() {
 
 	if lhsType == shape.CIRCLE && rhsType == shape.CIRCLE {
 		c.circleToCircle(c.lhs.Shape.(*circle.Circle), c.rhs.Shape.(*circle.Circle))
+	} else if lhsType == shape.CIRCLE && rhsType == shape.CONVEX {
+		c.circleToConvex(c.lhs.Shape.(*circle.Circle), c.rhs.Shape.(*convex.Convex))
+	} else if lhsType == shape.CONVEX && rhsType == shape.CIRCLE {
+		c.convexToCircle(c.lhs.Shape.(*convex.Convex), c.rhs.Shape.(*circle.Circle))
+	} else if lhsType == shape.CONVEX && rhsType == shape.CONVEX {
+		c.convexToConvex(c.lhs.Shape.(*convex.Convex), c.rhs.Shape.(*convex.Convex))
 	}
 }
 
@@ -32,4 +39,16 @@ func (c *Contact) circleToCircle(lhsCircle, rhsCircle *circle.Circle) {
 	c.penetration = radius - distance
 	c.normal.Normalize()
 	c.points = append(c.points, vector.Add(vector.Multiply(c.normal, lhsCircle.Radius), c.lhs.Position()))
+}
+
+func (c *Contact) circleToConvex(lhs *circle.Circle, rhs *convex.Convex) {
+
+}
+
+func (c *Contact) convexToCircle(lhs *convex.Convex, rhs *circle.Circle) {
+
+}
+
+func (c *Contact) convexToConvex(lhs, rhs *convex.Convex) {
+
 }
