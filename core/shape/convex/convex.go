3b22cbaf5646c96a1ccf2c8bee1dbb5e21eeb4ce
HueyPark <jaewan.huey.park@gmail.com>
2017-09-03 10:44:18 +0900

    Add quickhull algorithm to Convex

--- core/shape/convex/convex.go
+++ core/shape/convex/convex.go
@@ -1,18 +1,19 @@
 package convex
 
 import (
+	"math"
+
 	"github.com/hueypark/physics/core/shape"
 	"github.com/hueypark/physics/core/vector"
-	"math"
 )
 
 type Convex struct {
-	Vertices []vector.Vector
+	vertices []vector.Vector
+	hull     []vector.Vector
 }
 
 func New(vertices []vector.Vector) *Convex {
-	c := Convex{vertices}
-	c.nomalize()
+	c := Convex{vertices, nil}
 
 	return &c
 }
@@ -21,18 +22,79 @@ func (c *Convex) Type() int64 {
 	return shape.CONVEX
 }
 
-func (c *Convex) nomalize() {
-	leftExtreme := vector.Vector{math.MaxFloat64, 0}
-	rightExtreme := vector.Vector{-math.MaxFloat64, 0}
-	for _, v := range c.Vertices {
-		if leftExtreme.X < v.X {
-			leftExtreme = v
-		}
-
-		if v.X < rightExtreme.X {
-			rightExtreme = v
-		}
+func (c *Convex) Hull() []vector.Vector {
+	if c.hull == nil {
+		minX, maxX := c.getExtremePoints()
+		c.hull = append(c.quickHull(c.vertices, minX, maxX), c.quickHull(c.vertices, maxX, minX)...)
 	}
+
+	return c.hull
 }
 
-func (c *Convex)
+func (c *Convex) quickHull(points []vector.Vector, start, end vector.Vector) []vector.Vector {
+	pointDistanceIndicators := c.getLhsPointDistanceIndicatorMap(points, start, end)
+	if len(pointDistanceIndicators) == 0 {
+		return []vector.Vector{end}
+	}
+
+	farthestPoint := c.getFarthestPoint(pointDistanceIndicators)
+
+	newPoints := []vector.Vector{}
+	for point := range pointDistanceIndicators {
+		newPoints = append(newPoints, point)
+	}
+
+	return append(
+		c.quickHull(newPoints, start, farthestPoint),
+		c.quickHull(newPoints, farthestPoint, end)...)
+}
+
+func (c *Convex) getExtremePoints() (minX, maxX vector.Vector) {
+	minX = vector.Vector{math.MaxFloat64, 0}
+	maxX = vector.Vector{-math.MaxFloat64, 0}
+
+	for _, p := range c.vertices {
+		if p.X < minX.X {
+			minX = p
+		}
+
+		if maxX.X < p.X {
+			maxX = p
+		}
+	}
+
+	return minX, maxX
+}
+
+func (c *Convex) getLhsPointDistanceIndicatorMap(points []vector.Vector, start, end vector.Vector) map[vector.Vector]float64 {
+	pointDistanceIndicatorMap := make(map[vector.Vector]float64)
+
+	for _, point := range points {
+		distanceIndicator := c.getDistanceIndicator(point, start, end)
+		if distanceIndicator > 0 {
+			pointDistanceIndicatorMap[point] = distanceIndicator
+		}
+	}
+
+	return pointDistanceIndicatorMap
+}
+
+func (c *Convex) getDistanceIndicator(point, start, end vector.Vector) float64 {
+	vLine := vector.Subtract(end, start)
+
+	vPoint := vector.Subtract(point, start)
+
+	return vector.Cross(vPoint, vLine)
+}
+
+func (c *Convex) getFarthestPoint(pointDistanceIndicatorMap map[vector.Vector]float64) (farthestPoint vector.Vector) {
+	maxDistanceIndicator := -math.MaxFloat64
+	for point, distanceIndicator := range pointDistanceIndicatorMap {
+		if maxDistanceIndicator < distanceIndicator {
+			maxDistanceIndicator = distanceIndicator
+			farthestPoint = point
+		}
+	}
+
+	return farthestPoint
+}
--- core/shape/convex/convex_test.go
+++ core/shape/convex/convex_test.go
@@ -10,10 +10,10 @@ import (
 func TestNewConvex(t *testing.T) {
 	a := assert.New(t)
 
-	vertices := []vector.Vector{{-100, -100}, {100, -100}, {100, 100}, {-100, 100}, {0, 0}}
-	nomalizedVertices := []vector.Vector{{-100, -100}, {100, -100}, {100, 100}, {-100, 100}}
+	vertices := []vector.Vector{{0, 0}, {100, 0}, {100, -10}, {150, 100}, {100, 200}, {0, 210}, {-50, 100}, {30, 30}, {75, 30}}
+	hull := []vector.Vector{{0, 210}, {100, 200}, {150, 100}, {100, -10}, {0, 0}, {-50, 100}}
 
 	c := New(vertices)
 
-	a.Equal(c.Vertices, nomalizedVertices)
+	a.Equal(c.Hull(), hull)
 }
